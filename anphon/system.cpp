71d56d95da05d1061ce0aa2efe0fd9bd9caf472b
Terumasa Tadano <terumasa.tadano@gmail.com>
2016-07-12 22:00:13 +0900

    Removed cout entries for debug

--- anphon/system.cpp
+++ anphon/system.cpp
@@ -230,11 +230,6 @@ void System::setup()
         }
         cout << endl;
 
-        for (i = 0; i < nat; ++i) {
-            std::cout << std::setw(4) << i + 1;
-            std::cout << std::setw(4) << kd[i] << std::endl;
-        }
-
         memory->deallocate(xtmp);
 
         if (lspin) {
