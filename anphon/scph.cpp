b64d1c32f9dbe8142d9cf05fadb3781a14908d86
Terumasa Tadano <terumasa.tadano@gmail.com>
2017-11-02 23:56:51 +0900

    Added license header in scph.{cpp,h}

--- anphon/scph.cpp
+++ anphon/scph.cpp
@@ -1,3 +1,13 @@
+/*
+ scph.cpp
+
+ Copyright (c) 2015 Terumasa Tadano
+
+ This file is distributed under the terms of the MIT license.
+ Please see the file 'LICENCE.txt' in the root directory 
+ or http://opensource.org/licenses/mit-license.php for information.
+*/
+
 #include "mpi_common.h"
 #include "scph.h"
 #include "dynamical.h"
